58b64c5f8a46b45f49411d34a52f50e60ed4e5d2
Mustafa Soltekin <m.soltekin@reply.de>
2025-01-22 17:39:20 +0100

    Update for Day4

--- Day4/main.py
+++ Day4/main.py
@@ -1,1 +1,15 @@
 # print("randomization")
+class Human:
+    gender = "Female"
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} {self.age}"
+
+class Employee(Human):
+    pass
+
+mustafa = Employee("Mustafa","38")
+print(mustafa)
