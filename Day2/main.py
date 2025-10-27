7f86249d039c0a55d4856cd57656fcb3cb461c84
Mustafa Soltekin <52607828+msoltekin@users.noreply.github.com>
2025-10-28 00:26:09 +0100

    Day 2

---
+++ Day2/main.py
@@ -0,0 +1,17 @@
+print("Day2")
+print("Hello, World!"[-1])
+print(123_234_23_34)
+print(True)
+#name = input("Enter your name: ")
+#print("Hello, " + name)
+#age = int(input("Enter your age: "))
+#print("You are " + str(age) + " years old.")
+print(5//3)
+print(2**2)
+print(round(8/3, 2))
+
+# PEMDASLR
+# ()
+# **
+# * or /
+# + or -
